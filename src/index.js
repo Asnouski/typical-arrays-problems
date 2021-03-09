2614fe93c5e52fa470ea7c6acfa338ad1052d69f
asnouski <osnovvv@gmail.com>
2021-03-09 18:00:19 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+	if (!array || array.length === 0) return 0;
+	else return Math.min(...array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+	if (!array || array.length === 0) return 0;
+	else return Math.max(...array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+	if (!array || array.length === 0) return 0;
+	else {
+		let summa = array.reduce((sum, i) => { return sum + i }, 0)
+		return summa / array.length;
+	}
 }
+
